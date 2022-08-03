dbfa78176b6ceda18ada0bd2fc91ba89adef4796
Benassad <100342191+BenAssad@users.noreply.github.com>
2022-08-03 20:46:29 +0200

    about

--- src/Form/BookTableType.php
+++ src/Form/BookTableType.php
@@ -4,6 +4,10 @@ namespace App\Form;
 
 use App\Entity\BookTable;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,10 +16,26 @@ class BookTableType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('phoneNumber')
-            ->add('email')
-            ->add('howManyPersons')
+            ->add('name', TextType::class, [
+
+            ])
+            ->add('phoneNumber', TextType::class, [
+
+            ])
+            ->add('email', EmailType::class, [
+                
+            ])
+            ->add('howManyPersons', ChoiceType::class, [
+                'label' => 'Combien des personnes',
+                'choices' =>[
+                    'Combien des personnes' => 0,
+                    '1' => 1,
+                    '2' => 2,
+                    '3' => 3,
+                    '4' => 4,
+                ]
+            ])
+            ->add('Envoyer', SubmitType::class)
         ;
     }
 
