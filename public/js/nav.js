d011c7f0cca4eb42dc2df264cc6cbc46020d40b1
Benassad <100342191+BenAssad@users.noreply.github.com>
2022-10-09 12:06:04 +0200

    js nav

--- public/js/nav.js
+++ public/js/nav.js
@@ -3,6 +3,8 @@ let menu = document.getElementById('imenu');
 let nav = document.getElementById('menu');
 let menuri = document.getElementById('menuri');
 let pro = document.querySelector('.pro');
+let bar3 = document.getElementById('bar3');
+let x = document.getElementById('x');
 
 menu.addEventListener("click" , function(){
 
@@ -12,14 +14,15 @@ menu.addEventListener("click" , function(){
         pro.style.display = "block";
         nav.style.height = "400px";
         menu.setAttribute("aria-expanded", "true");
-        // menu.innerHTML = "<p>X</p>";
+        x.style.display = "block";
+        bar3.style.display = "none";
     }else{
         menuri.style.display = "none";
         pro.style.display = "none";
         nav.style.height = "70px";
-        // menu.innerHTML = <i data-feather="menu"></i>;
         menu.setAttribute("aria-expanded", "false");
-
+        x.style.display = "none";
+        bar3.style.display = "block";
     }
 
     // console.log("Hello");
